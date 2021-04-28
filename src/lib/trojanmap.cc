62762ecbcdb66287ff4e65f9fd25a53a3cbc357c
Guangrui <guangrui@usc.edu>
2021-04-27 22:46:23 -0700

    submission

--- src/lib/trojanmap.cc
+++ src/lib/trojanmap.cc
@@ -131,8 +131,8 @@ void TrojanMap::PrintMenu() {
     std::string input2;
     getline(std::cin, input2);
     auto start = std::chrono::high_resolution_clock::now();
-    // auto results = CalculateShortestPath_Dijkstra(input1, input2);
-    auto results = CalculateShortestPath_Bellman_Ford(input1, input2);
+    auto results = CalculateShortestPath_Dijkstra(input1, input2);
+    // auto results = CalculateShortestPath_Bellman_Ford(input1, input2);
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
     menu = "*************************Results******************************\n";
